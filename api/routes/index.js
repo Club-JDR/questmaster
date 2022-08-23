0f98acd6465dc203773dcd8723be6a53c420b8ff
Maxime Gaston <maxime.gaston@cardiologs.com>
2022-08-23 10:02:32 +0200

    Better uptime

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const dateUtils = require('../utils/date');
 
 router.get('/health', function(req, res, next) {
     res.send({
         title: 'QuestMaster API',
         version: 1,
         status: 'OK',
-        uptime: Math.round(process.uptime()),
+        uptime: dateUtils(Math.round(process.uptime())),
         date: new Date()
     });
 });
