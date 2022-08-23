51e52e59fe351ccfa28b2154661be4596eef938b
Maxime Gaston <maxime.gaston@cardiologs.com>
2022-08-23 10:44:32 +0200

    Fix uptime

--- api/controllers/health.js
+++ api/controllers/health.js
@@ -1,11 +1,13 @@
 const dateUtils = require('../utils/date');
 
-const healthCheck = {
-    title: 'QuestMaster API',
-    version: 1,
-    status: 'OK',
-    uptime: dateUtils(Math.round(process.uptime())),
-    date: new Date()
+function healthCheck() {
+    return {
+        title: 'QuestMaster API',
+        version: 1,
+        status: 'OK',
+        uptime: dateUtils(Math.round(process.uptime())),
+        date: new Date()
+    }
 }
 
 module.exports = healthCheck;
--- api/routes/health.js
+++ api/routes/health.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const healthCheck = require('../controllers/health');
 
 router.get('/', function(req, res, next) {
-    res.send(healthCheck);
+    res.send(healthCheck());
 });
 
 module.exports = router;
