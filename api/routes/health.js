20ea3026785099c657c28e1300ce82d4f1d59948
Maxime Gaston <maxime.gaston@cardiologs.com>
2022-08-23 15:21:51 +0200

    Refactor

--- api/routes/health.js
+++ api/routes/health.js
@@ -3,8 +3,7 @@ const router = express.Router();
 const healthCheck = require('../controllers/health');
 
 router.get('/', function(req, res, next) {
-    res.setHeader('content-type', 'application/json');
-    res.send(healthCheck());
+    res.setHeader('content-type', 'application/json').send(healthCheck());
 });
 
 module.exports = router;
