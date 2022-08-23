27355cc5c90cdd711c67aaf0e1a5ba39b4a31d42
Maxime Gaston <maxime.gaston@cardiologs.com>
2022-08-23 09:40:10 +0200

    Add health endpoint

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.send({ title: 'Express App running', version: 1 });
+router.get('/health', function(req, res, next) {
+    res.send({
+        title: 'QuestMaster API',
+        version: 1,
+        status: 'OK',
+        uptime: Math.round(process.uptime()),
+        date: new Date()
+    });
 });
 
 module.exports = router;
