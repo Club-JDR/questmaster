88de678f5b915b3007c9c0db73927e92701ede45
Maxime Gaston <maxime.gaston@cardiologs.com>
2023-04-21 22:40:35 +0200

    Improve stuff

--- website/views/filters.py
+++ website/views/filters.py
@@ -3,9 +3,9 @@ import locale, datetime
 
 
 @app.template_filter("format_datetime")
-def format_datetime(value, format="%A %d %B à %H:%M"):
+def format_datetime(value, format="%a %d/%m - %Hh%M"):
     """
-    Jinja filter to format date like this: Samedi 10 Juin à 20:30
+    Jinja filter to format date like this: Sam 10/09 - 20h30
     """
     locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
     if isinstance(value, str):
