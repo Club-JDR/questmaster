dcbb1a9c6db4bb49e6b8ce614a03509580b06719
Maxime Gaston <maxime.gaston@cardiologs.com>
2023-04-22 14:51:01 +0200

    Fix test filter

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -7,6 +7,6 @@ def test_format_datetime():
     Test format_datetime filter.
     """
     date = datetime.datetime(2000, 1, 1, 0, 1)
-    f_date = format_datetime(date)
-    expected_date = "Sam 01/01 - 00h01"
+    f_date = format_datetime(date).lower()
+    expected_date = "sam 01/01 - 00h01"
     assert expected_date == f_date
