8aa5e10e26184095e4d3073d330413f32144e28a
notsag <maxime@gaston.sh>
2025-07-14 15:16:45 +0200

    Fix missing pp

--- website/models/user.py
+++ website/models/user.py
@@ -3,7 +3,7 @@ from sqlalchemy import orm
 from flask import current_app
 from website.bot import get_bot
 from website.utils.logger import logger
-import re
+import re, requests
 
 AVATAR_BASE_URL = "https://cdn.discordapp.com/avatars/{}/{}"
 
@@ -47,6 +47,7 @@ class User(db.Model):
         Retrieve distant data on user when the oject is loaded.
         """
         result = get_user(self.id)
+        self.avatar = "/static/img/avatar.webp"
         try:
             if result["nick"] == None:
                 if result["user"]["global_name"] == None:
@@ -62,13 +63,15 @@ class User(db.Model):
             self.is_player = (
                 current_app.config["DISCORD_PLAYER_ROLE_ID"] in result["roles"]
             )
-            if "None" in result["user"]["avatar"]:
-                self.avatar = "/static/img/avatar.webp"
-            else:
-                self.avatar = AVATAR_BASE_URL.format(self.id, result["user"]["avatar"])
+            avatar_url = AVATAR_BASE_URL.format(self.id, result["user"]["avatar"])
+            try:
+                response = requests.head(avatar_url)
+                if response.status_code == 200:
+                    self.avatar = avatar_url
+            except requests.RequestException:
+                pass
         except Exception:
             self.name = "Inconnu"
             self.is_gm = False
             self.is_admin = False
             self.is_player = False
-            self.avatar = "/static/img/avatar.webp"
