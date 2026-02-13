4d5c87fe04f15c8584a4d1b17326af7dfc6a4afa
notsag <maxime@gaston.sh>
2026-02-13 13:30:55 +0100

    fix(deps): upgrade to flask-admin 2.0.2

--- website/__init__.py
+++ website/__init__.py
@@ -94,8 +94,6 @@ def create_app():
     admin = Admin(
         app,
         name="QuestMaster Admin",
-        template_mode="bootstrap4",
-        base_template="admin.j2",
         index_view=admin_view.SecureAdminIndexView(),
     )
     admin.add_view(admin_view.GameAdmin(models.Game, db.session, name="Annonces"))
